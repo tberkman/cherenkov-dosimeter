289fc4c54bc7d563768aada99d22a1109f98a05d
tberkman <theoberk@haverford.org>
2026-07-20 16:37:47 -0400

    Added optical Cherenkov radiation sim (Geant4) - will notebook later

---
+++ Geant4/geant4-sim/Optical/src/DetectorConstruction.cc
@@ -0,0 +1,141 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+/// \file DetectorConstruction.cc
+/// \brief Implementation of the cherenkov::DetectorConstruction class
+
+#include "../include/DetectorConstruction.hh"
+
+#include "G4Box.hh"
+#include "G4Cons.hh"
+#include "G4LogicalVolume.hh"
+#include "G4NistManager.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4Trd.hh"
+#include "G4MaterialPropertiesTable.hh"
+#include <vector>
+
+
+
+namespace cherenkov
+{
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+  // Get nist material manager
+  G4NistManager* nist = G4NistManager::Instance();
+
+  // Envelope parameters
+  //
+  G4double env_sizeXY = 20 * cm, env_sizeZ = 30 * cm;
+  G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
+  std::vector<G4double> photonEnergy = {
+  1.771 * eV,   // 700 nm
+  2.066 * eV,   // 600 nm
+  2.254 * eV,   // 550 nm
+  2.480 * eV,   // 500 nm
+  2.755 * eV,   // 450 nm
+  3.100 * eV,   // 400 nm
+  3.542 * eV,   // 350 nm
+  4.133 * eV    // 300 nm
+};
+
+std::vector<G4double> rindex = {
+  1.331,   // 700 nm
+  1.334,   // 600 nm
+  1.335,   // 550 nm
+  1.337,   // 500 nm
+  1.340,   // 450 nm
+  1.343,   // 400 nm
+  1.348,   // 350 nm
+  1.349    // 300 nm
+};
+  auto waterMPT = new G4MaterialPropertiesTable();
+  waterMPT->AddProperty("RINDEX", photonEnergy, rindex);
+  env_mat->SetMaterialPropertiesTable(waterMPT);
+  // Option to switch on/off checking of volumes overlaps
+  //
+  G4bool checkOverlaps = true;
+
+  //
+  // World
+  //
+  G4double world_sizeXY = 1.2 * env_sizeXY;
+  G4double world_sizeZ = 1.2 * env_sizeZ;
+  G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
+
+  auto solidWorld =
+    new G4Box("World",  // its name
+              0.5 * world_sizeXY, 0.5 * world_sizeXY, 0.5 * world_sizeZ);  // its size
+
+  auto logicWorld = new G4LogicalVolume(solidWorld,  // its solid
+                                        world_mat,  // its material
+                                        "World");  // its name
+
+  auto physWorld = new G4PVPlacement(nullptr,  // no rotation
+                                     G4ThreeVector(),  // at (0,0,0)
+                                     logicWorld,  // its logical volume
+                                     "World",  // its name
+                                     nullptr,  // its mother  volume
+                                     false,  // no boolean operation
+                                     0,  // copy number
+                                     checkOverlaps);  // overlaps checking
+
+  //
+  // Envelope
+  //
+  auto solidEnv = new G4Box("Envelope",  // its name
+                            0.5 * env_sizeXY, 0.5 * env_sizeXY, 0.5 * env_sizeZ);  // its size
+
+  auto logicEnv = new G4LogicalVolume(solidEnv,  // its solid
+                                      env_mat,  // its material
+                                      "Envelope");  // its name
+
+  new G4PVPlacement(nullptr,  // no rotation
+                    G4ThreeVector(),  // at (0,0,0)
+                    logicEnv,  // its logical volume
+                    "Envelope",  // its name
+                    logicWorld,  // its mother  volume
+                    false,  // no boolean operation
+                    0,  // copy number
+                    checkOverlaps);  // overlaps checking
+
+  //
+
+
+  fScoringVolume = logicEnv;
+
+  //
+  // always return the physical World
+  //
+  return physWorld;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+}  // namespace cherenkov
